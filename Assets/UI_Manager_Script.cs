3455b922ff5b1d146dbf67201239eb2fa8d1893f
Giuseppe Fiorillo <giuseppe.fiorillo001@studenti.uniparthenope.it>
2023-09-21 22:09:07 +0200

    Fix

---
+++ Assets/UI_Manager_Script.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager_Script : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] _livesSprites; // Array di sprite per rappresentare le vite
+
+    [SerializeField]
+    private Image _livesImage;
+
+    public void updateLives(int currentLives)
+    {
+        _livesImage.sprite = _livesSprites[currentLives];
+    }
+
+}
